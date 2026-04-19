5c453a15c67509b40b2cd82250660cff3114ed5c
Darko <dark.araya@duocuc.cl>
2026-04-19 01:00:19 -0400

    feat: modelo Usuario, repositorio y configuración MySQL

--- src/main/java/ddd/void_archive/Model/Usuario.java
+++ src/main/java/ddd/void_archive/Model/Usuario.java
@@ -1,5 +1,49 @@
-package ddd.void_archive.Model;
+package ddd.void_archive.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String contraseña;
+
+    private String telefono;
+    private String direccion;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+    private LocalDateTime fechaRegistro;
+
+    public void prePersist() {
+        this.fechaRegistro = LocalDateTime.now();
+        if (this.rol == null) {
+            this.rol = Rol.CLIENTE; // Asignar rol por defecto
+        }
+    }
+    public enum Rol{
+        CLIENTE, ADMIN
+    }
 }
---
+++ src/main/java/ddd/void_archive/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package ddd.void_archive.repository;
+
+import ddd.void_archive.model.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+    
+
+}
